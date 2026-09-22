c23a99408e16f385879438704564d265b409b47c
CooperSobey <csobey@ualberta.ca>
2026-09-21 18:42:00 -0600

    Done task 1 and 2

--- src/list_vs_array.cpp
+++ src/list_vs_array.cpp
@@ -1,4 +1,7 @@
 #include <cstddef>  // size_t
+#include <iostream>
+#include <list>
+#include <random>
 
 #include "timer.h"
 
@@ -6,5 +9,37 @@ constexpr size_t SIZE = 16000000;
 
 int main() {
     // test here...
+    Timer timer;
+    std::list<uint64_t> list;
+    std::vector<uint64_t> vector;
+    std::vector<uint64_t> vector_reserved;
+    std::mt19937_64 rng(0);
+    uint64_t next;
+
+    rng.seed(0);
+    timer.restart();
+    for (size_t i = 0; i < SIZE; ++i) {
+        next = rng();
+        list.push_back(next);
+    }
+    std::cout << "List: " << timer.glance<Timer::microseconds>() << std::endl;
+
+    rng.seed(0);
+    timer.restart();
+    for (size_t i = 0; i < SIZE; ++i) {
+        next = rng();
+        vector.push_back(next);
+    }
+    std::cout << "Normal Vector: " << timer.glance<Timer::microseconds>() << std::endl;
+
+    vector_reserved.reserve(SIZE);
+    rng.seed(0);
+    timer.restart();
+    for (size_t i = 0; i < SIZE; ++i) {
+        next = rng();
+        vector_reserved.push_back(next);
+    }
+    std::cout << "Reserved Vector: " << timer.glance<Timer::microseconds>() << std::endl;
+
     return 0;
 }
--- src/timer.h
+++ src/timer.h
@@ -1,4 +1,43 @@
 #ifndef TIMER_H
 #define TIMER_H
+#include <chrono>
+#include <cstdint>
+#include <ratio>
 
+
+
+class Timer {
+public:
+    using nanoseconds = std::chrono::nanoseconds;
+    using microseconds = std::chrono::microseconds;
+    using milliseconds = std::chrono::milliseconds;
+    using seconds = std::chrono::seconds;
+    using minutes = std::chrono::minutes;
+    using hours = std::chrono::hours;
+
+    Timer() : lastTime(std::chrono::steady_clock::now()) {}
+
+    void restart() {
+        lastTime = std::chrono::steady_clock::now();
+    }
+
+    template <typename T>
+    uint64_t click() {
+        const auto now = std::chrono::steady_clock::now();
+        auto elapsed = std::chrono::duration_cast<T>(now - lastTime);
+        lastTime = now;
+        return elapsed.count();
+    }
+
+    template <typename T>
+    uint64_t glance() const {
+        const auto now = std::chrono::steady_clock::now();
+        auto elapsed = std::chrono::duration_cast<T>(now - lastTime);
+        return elapsed.count();
+    }
+
+private:
+    std::chrono::steady_clock::time_point lastTime;
+
+};
 #endif  // TIMER_H
